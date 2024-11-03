483a74c521a9d3207201c57db2597f66ffb794e9
T00708314 <T00708314@OM1791-06>
2024-11-03 14:36:26 -0800

    Added queue, adjusted size of root window

--- lab8demoCode/lab8demoCode/lab8demoCode.py
+++ lab8demoCode/lab8demoCode/lab8demoCode.py
@@ -3,5 +3,6 @@ from tkinter import messagebox
 
 root = tk.Tk();
 root.title("Start Window")
+root.geometry("600x700")
 
 root.mainloop()
---
+++ lab8demoCode/lab8demoCode/myQueue.py
@@ -0,0 +1,14 @@
+class myQueue:
+    def __init__(self):
+        self.content = []
+
+    def empty(self):
+        return len(self.content) == 0
+
+    def push(self, item):
+        self.content.append(item)
+
+    def pop(self):
+        if self.empty():
+            return "Is empty"
+        return self.content.pop(0)
